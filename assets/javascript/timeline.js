c836190a357c553792f603a883fa4e35a3299c82
SITRUK <kurtis.santillanes@gmail.com>
2019-04-28 12:10:46 -0700

    good progress with the film reel effect CSS. Next step to fix the js file to not just append, but remove extra blanks for film effect. general clickbility of js function needs to be changed as well. while adding functionaliy, will add touch for mobile responsiveness. currently only optimize for mousemove.

--- assets/javascript/timeline.js
+++ assets/javascript/timeline.js
@@ -1,29 +1,37 @@
 var viewportWidth, divWidth, tb;
-	$(function() {
-		
-		viewport = $('#container').innerWidth();
-		tb = $('#thumbs');
-		divWidth = tb.outerWidth();
-    
-		$('#container').mousemove(function(e)
-		{
-      tb.css({left: ((viewport - divWidth)*((e.pageX / viewport).toFixed(3))).toFixed(1) +"px" });
- 		});
-    
-    $('.history-block').on('click', function() {
-      $('.history-block').css('width', '300px');
-      $('.history-block').find('.title').css('width', '260px');
-       $('.history-block .timeline').hide(300);
-        $(this).css('width', '600px');
-        $(this).find('.title').css('width', '500px');
-       $(this).find('.timeline').show(800);
-      $('#container').mousemove(function(e)
-        {
-          tb.css({left: ((viewport - divWidth-300)*((e.pageX / viewport).toFixed(3))).toFixed(1) + 300 + "px" });
-          });
+$(function () {
+
+  viewport = $('#timelineContainer').innerWidth();
+  tb = $('#thumbs');
+  divWidth = tb.outerWidth();
+
+  $('#timelineContainer').mousemove(function (e) {
+    tb.css({ left: ((viewport - divWidth) * ((e.pageX / viewport).toFixed(3))).toFixed(1) + "px" });
+  });
+
+  $('.history-block').on('click', function () {
+    $('.history-block').css('width', '300px');
+    $('.history-block').find('.title').css('width', '260px');
+    $('.history-block .timeline').hide(300);
+    $(this).css('width', '600px');
+    $(this).find('.title').css('width', '500px');
+    $(this).find('.timeline').show(800);
+    $(this).find('.top-boxes').append(`<span class="small_box"></span>
+                                        <span class="small_box"></span>
+                                        <span class="small_box"></span>
+                                        <span class="small_box"></span>
+                                        <span class="small_box"></span>`);
+    $(this).find('.bottom-boxes').append(`<span class="small_box"></span>
+                                        <span class="small_box"></span>
+                                        <span class="small_box"></span>
+                                        <span class="small_box"></span>
+                                        <span class="small_box"></span>`);
+    $('#timelineContainer').mousemove(function (e) {
+      tb.css({ left: ((viewport - divWidth - 300) * ((e.pageX / viewport).toFixed(3))).toFixed(1) + 300 + "px" });
     });
-    
-    $('.timeline ul li').on('click', function(){
-        $(this).parent().blink();
-    });
+  });
+
+  $('.timeline ul li').on('click', function () {
+    $(this).parent().blink();
+  });
 });

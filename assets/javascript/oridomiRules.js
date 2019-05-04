1e10270bfae136f34ab8063faf3357bb85c1258d
SITRUK <kurtis.santillanes@gmail.com>
2019-05-04 12:53:56 -0700

    fixed the oridomi bug with a toggle class button

--- assets/javascript/oridomiRules.js
+++ assets/javascript/oridomiRules.js
@@ -1,21 +1,24 @@
 // all of the customizations that are available
 var folded = new OriDomi('.paper', {
-    vPanels:          [20, 10, 10, 20, 10, 20, 10],     // number of panels when folding left or right (vertically oriented)
-    hPanels:          5,     // number of panels when folding top or bottom
-    speed:            1200,  // folding duration in ms
-    ripple:           2,     // backwards ripple effect when animating
+    vPanels: [20, 10, 10, 20, 10, 20, 10],     // number of panels when folding left or right (vertically oriented)
+    hPanels: 5,     // number of panels when folding top or bottom
+    speed: 1200,  // folding duration in ms
+    ripple: 2,     // backwards ripple effect when animating
     shadingIntensity: .5,    // lessen the shading effect
-    perspective:      800,   // smaller values exaggerate 3D distortion
-    maxAngle:         40,    // keep the user's folds within a range of -40 to 40 degrees
-    touchEnabled:     true,  // enable touch
-    shading:          'hard' // change the shading type
+    perspective: 800,   // smaller values exaggerate 3D distortion
+    maxAngle: 40,    // keep the user's folds within a range of -40 to 40 degrees
+    touchEnabled: true,  // enable touch
+    shading: 'hard' // change the shading type
+});
+
+$('#seeResume').on('click', function () {
+    $('#resume').toggle('active');
+    folded.reveal(100, 'top');
   });
 
-  folded.reveal(10, 'top');
-
-  $('#seeResume').on('click', function () {
-    $('#resume').find('.timeline').toggle();
-    $(this).find('.project').toggleClass('active');
-    $(this).find('.top-boxes-zoom').toggle();
-    $(this).find('.bottom-boxes-zoom').toggle();
-  });
+//   $('#seeResume').on('click', function () {
+//     const foldAndUnfold = folded.foldUp();
+//     $(this).find('#resume').toggleClass('active');
+//     $('#resume').toggleClass('active');
+//     if ()
+//   });
